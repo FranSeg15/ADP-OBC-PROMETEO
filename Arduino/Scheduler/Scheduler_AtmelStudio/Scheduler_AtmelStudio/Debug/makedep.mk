3612895b8da450941b638bc2cfdea3cbd30860cb
FranSeg15 <42903704+FranSeg15@users.noreply.github.com>
2020-08-01 00:51:42 -0600

    Added  atmel studio files

---
+++ Arduino/Scheduler/Scheduler_AtmelStudio/Scheduler_AtmelStudio/Debug/makedep.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+LED.cpp
+
+Mailbox.cpp
+
+main.cpp
+
+Scheduler.cpp
+
+Task.cpp
+
